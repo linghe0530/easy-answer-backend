cb08c22e9cfafb3e3baf019175306862680b79e4
linghe <2286340149@qq.com>
2025-09-17 17:38:35 +0800

    第一次提交

---
+++ src/main/java/com/crane/answer/Main.java
@@ -0,0 +1,12 @@
+package com.crane.answer;
+
+/**
+ * @author crane
+ * @date 2025.09.17 下午5:31
+ * @description
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
